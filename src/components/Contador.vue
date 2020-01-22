343e71a626ef17f80e90f6edb39ea5d585a3ef27
italofelipe <felipe.italo@hotmail.com>
2020-01-22 11:19:28 -0300

    Implementados computed properties de diferentes maneiras, para ilustrar formas de se alcançar o State do Vuex. Assim como no React, apesar de termos um State centralizado, é completamente possível termos também um "state local" usando o Data( ).

--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -1,6 +1,8 @@
 <template>
 	<div>
 		<h2 class="font-weight-light">Contador: {{ contador }}</h2>
+		<h4 class="font-weight-light">Contador Alias: {{ contadorAlias }}</h4>
+		<h4 class="font-weight-light">Contador Multiplicado: {{ contadorMultiplicado }}</h4>
 		<button class="btn btn-sm m-2 btn-danger" @click="decrementar">
 			Decrementar
 		</button>
@@ -11,24 +13,47 @@
 </template>
 
 <script>
+import { mapState } from "vuex";
 export default {
-	name: "Contador",
-  
+  name: "Contador",
+  // Adicionalmente ao estado global do Vuex, podemos ter instâncias locais de data
+  data() {
+    return {
+      contadorLocal: 2
+    }
+  },
+
 	methods: {
 		decrementar() {
-      this.$store()
+			
 			this.$store.state.contador--;
 		},
 		incrementar() {
 			this.$store.state.contador++;
 		}
-  },
-  // Essa computed (contador) é basicamente uma "referência"
-  // para $store.state.
-  computed: {
-    contador() {
-      return this.$store.state.contador
-    } 
-  }
+	},
+	// Essa computed (contador) é basicamente uma "referência"
+	// para $store.state.
+   computed: mapState({
+      contador: 'contador', /* -> Note que a chave é o que usamos nesse componente,
+       e o valor é algo que está dentro do State no Vuex */
+       contadorAlias: 'contador',
+       contadorMultiplicado(state) {
+         return state.contador * this.contadorLocal
+       }
+    })  	
+  /*
+    Alternativamente, poderíamos re-escrever essa computed
+    das seguints maneiras:
+  
+    1.
+    computed: mapState({
+      contador: state => state.contador
+    }) 	
+
+    2.
+
+    mapState(['contador'])
+  */
 };
 </script>
