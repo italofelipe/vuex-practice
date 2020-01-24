3b59009c5f52b68761294e1f4163b7c02a4e428b
italofelipe <felipe.italo@hotmail.com>
2020-01-24 15:44:12 -0300

    Implementado getters nos componentes.

--- src/components/TarefasLista.vue
+++ src/components/TarefasLista.vue
@@ -1,67 +1,89 @@
 <template>
-  <div>
-    <div class="row">
-      <div class="col-sm-10">
-        <h1 class="font-weight-light">Lista de Tarefas</h1>
-      </div>
-      <div class="col-sm-2">
-        <button class="btn btn-primary float-right" @click="exibirFormularioCriarTarefa">
-          <i class="fa fa-plus mr-2"></i>
-          <span>Criar</span>
-        </button>
-      </div>
-    </div>
+	<div>
+		<div class="row">
+			<div class="col-sm-10">
+				<h1 class="font-weight-light">Lista de Tarefas</h1>
+			</div>
+			<div class="col-sm-2">
+				<button
+					class="btn btn-primary float-right"
+					@click="exibirFormularioCriarTarefa"
+				>
+					<i class="fa fa-plus mr-2"></i>
+					<span>Criar</span>
+				</button>
+			</div>
+		</div>
 
-    <ul class="list-group" v-if="tarefas.length > 0">
-      <TarafasListaItem
-        v-for="tarefa in tarefas"
-        :key="tarefa.id"
-        :tarefa="tarefa"
-        @editar="selecionarTarefaParaEdicao"
-      />
-    </ul>
-    <p v-else>Nenhuma tarefa criada.</p>
-    <TarafaSalvar v-if="exibirFormulario" :tarefa="tarefaSelecionada" />
-  </div>
+		<h3 class="font-weight-light mt-4">
+			A fazer: ({{ $store.getters.tarefasAFazer.length }})
+		</h3>
+
+		<ul class="list-group" v-if="$store.getters.tarefasAFazer.length > 0">
+			<TarafasListaItem
+				v-for="tarefa in $store.getters.tarefasAFazer"
+				:key="tarefa.id"
+				:tarefa="tarefa"
+				@editar="selecionarTarefaParaEdicao"
+			/>
+		</ul>
+		<p v-else>Nenhuma tarefa a fazer.</p>
+
+
+    <h3 class="font-weight-light mt-4">
+			Concluídas: ({{ $store.getters.totalTarefasConcluidas }})
+		</h3>
+
+		<ul class="list-group" v-if="$store.getters.tarefasConcluidas.length > 0">
+			<TarafasListaItem
+				v-for="tarefa in $store.getters.tarefasConcluidas"
+				:key="tarefa.id"
+				:tarefa="tarefa"
+				@editar="selecionarTarefaParaEdicao"
+			/>
+		</ul>
+		<p v-else>Nenhuma tarefa Foi concluída</p>
+
+		<TarafaSalvar v-if="exibirFormulario" :tarefa="tarefaSelecionada" />
+	</div>
 </template>
 
 <script>
-import { mapState} from 'vuex'
+import { mapState } from "vuex";
 import TarafasListaItem from "./TarafasListaItem";
 import TarafaSalvar from "./TarafaSalvar";
 export default {
-  name: "TarefasLista",
-  components: {
-    TarafasListaItem,
-    TarafaSalvar
-  },
-  data() {
-    return {
-      exibirFormulario: false,
-      tarefaSelecionada: undefined
-      
-    };
-  },
-  methods: {
-    exibirFormularioCriarTarefa() {
-      // Verificar se ja temos uma tarefa selecionada (ja temos isso lcoalmente no Data)
-      if (this.tarefaSelecionada) {
-        this.tarefaSelecionada = undefined;
-        return;
-      }
-      this.exibirFormulario = !this.exibirFormulario;
-    },
-    selecionarTarefaParaEdicao(tarefa) {
-      this.exibirFormulario = true;
-      this.tarefaSelecionada = tarefa;
-    },
-    resetar() {
-      this.exibirFormulario = false;
-      this.tarefaSelecionada = undefined;
-    }
-  },
-  computed: {
-    ...mapState(['tarefas'])
-  }
+	name: "TarefasLista",
+	components: {
+		TarafasListaItem,
+		TarafaSalvar
+	},
+	data() {
+		return {
+			exibirFormulario: false,
+			tarefaSelecionada: undefined
+		};
+	},
+	methods: {
+		exibirFormularioCriarTarefa() {
+			// Verificar se ja temos uma tarefa selecionada (ja temos isso lcoalmente no Data)
+			if (this.tarefaSelecionada) {
+				this.tarefaSelecionada = undefined;
+				return;
+			}
+			this.exibirFormulario = !this.exibirFormulario;
+		},
+		selecionarTarefaParaEdicao(tarefa) {
+			this.exibirFormulario = true;
+			this.tarefaSelecionada = tarefa;
+		},
+		resetar() {
+			this.exibirFormulario = false;
+			this.tarefaSelecionada = undefined;
+		}
+	},
+	computed: {
+		...mapState(["tarefas"])
+	}
 };
 </script>
--- src/store.js
+++ src/store.js
@@ -1,17 +1,36 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    contador: 0,
-    tarefas: [
-      { id: 1, titulo: "Aprender Vue.js", concluido: true },
-      { id: 2, titulo: "Aprender Vuex", concluido: false },
-      { id: 3, titulo: "Aprender Vue Router", concluido: false }
-    ],
-    nome: "A message from inside the Vuex Store. Nice, isn'/t it? "
-
-  }
-})
+	state: {
+		contador: 0,
+		tarefas: [
+			{ id: 1, titulo: "Aprender Vue.js", concluido: true },
+			{ id: 2, titulo: "Aprender Vuex", concluido: true },
+			{ id: 3, titulo: "Aprender Vue Router", concluido: true },
+			{ id: 3, titulo: "Aprender GraphQL", concluido: false },
+			{ id: 3, titulo: "Aprender React Native", concluido: false }
+		],
+		nome: "A message from inside the Vuex Store. Nice, isn'/t it? "
+	},
+	/* Getters no Vuex são uma forma de se fazer uma tratativa em cima dos dados 
+    que temos no estado Global da aplicação. No exemplo abaixo, fiz uma tratativa
+    para filtrar somente pelas  tarefas concluídas (e dps fiz outra tratativa para 
+    filtrar pelas tarefas incompletas.) Desse modo, tiramos a responsabilidade dos
+    componentes de terem que fazer esse trabalho.
+  */
+	getters: {
+		//
+		tarefasConcluidas: state => {
+			return state.tarefas.filter(tarefa => tarefa.concluido === true);
+		},
+		tarefasAFazer: state => {
+			return state.tarefas.filter(tarefa => tarefa.concluido === false);
+		},
+		totalTarefasConcluidas: (state, getters) => {
+			return getters.tarefasConcluidas.length;
+		}
+	}
+});
